ad6761ffbe886fe45e031876ea0e287a489758c2
gpdzvapatsva <gpdzvapatsva@gmail.com>
2022-09-27 19:20:53 +0200

    icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() =>  runApp(MaterialApp(
-  home:Home(),
-));
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+void main() =>  runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: "My Flutter App",
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
       appBar: AppBar(
-        title: Text("Stateless Widget"),
+        title: const Text("Stateless Widget"),
         backgroundColor: Colors.purple[100],
         centerTitle: true,
       ),
-      body: Center(
-        child:Image(
-          image: AssetImage('download1.jfif'),
-        ),
+      body: Center(child:IconButton(
+        icon: const Icon(
+          Icons.alt_route_outlined
+        ),onPressed: (){},
+        iconSize: 100,
+        color:Colors.blueGrey[100],
+        splashColor: Colors.white,
+        alignment: Alignment.center,
+        highlightColor: Colors.red,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){},
-        child:Text('Click Meyy'),
-      ),
-    );
+
+          ),
+        ));
   }
 }
 
